4506656b85ac4c2d58e3e63111bfd960ea59787c
GuardianBob <jgmeyer82@gmail.com>
2026-05-28 07:48:50 -0700

    update to fix hover in light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,15 @@ module.exports = {
       colors: {
         "cyber-slate": "#adadad",
         "primary": "#c8c8c8",
-        "white": "var(--white)",
-        "neon-cyan": "var(--neon-cyan)",
-        "neon-green": "var(--neon-green)",
-        "neon-pink": "var(--neon-pink)",
-        "neon-purple": "var(--neon-purple)",
-        "neon-orange": "var(--neon-orange)",
-        "surface": "var(--surface)",
-        "background": "var(--bg)",
+        "white": "rgb(var(--white-rgb) / <alpha-value>)",
+        "neon-cyan": "rgb(var(--neon-cyan-rgb) / <alpha-value>)",
+        "neon-green": "rgb(var(--neon-green-rgb) / <alpha-value>)",
+        "neon-pink": "rgb(var(--neon-pink-rgb) / <alpha-value>)",
+        "neon-purple": "rgb(var(--neon-purple-rgb) / <alpha-value>)",
+        "neon-orange": "rgb(var(--neon-orange-rgb) / <alpha-value>)",
+        "surface": "rgb(var(--surface-rgb) / <alpha-value>)",
+        "background": "rgb(var(--bg-rgb) / <alpha-value>)",
+        "border-hi": "rgb(var(--border-hi-rgb) / <alpha-value>)",
         "hover-light": "var(--hover-light)"
       }
     }
